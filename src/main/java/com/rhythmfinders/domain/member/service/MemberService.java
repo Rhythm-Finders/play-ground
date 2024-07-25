11c0adb9384d9a79ce0b230043cb9887f91f898e
JeongJunSeo99 <wnstj0810@naver.com>
2024-07-26 00:16:44 +0900

    feat MemberService Application에서 사용하는 MemberService method 추가

--- src/main/java/com/rhythmfinders/domain/member/service/MemberService.java
+++ src/main/java/com/rhythmfinders/domain/member/service/MemberService.java
@@ -4,4 +4,32 @@ import com.rhythmfinders.domain.member.repository.MemberRepository;
 
 public class MemberService {
     private MemberRepository memberRepository = new MemberRepository();
+
+    public void signUp() {
+    }
+
+    public int login() {
+        return 0;
+    }
+
+    public void findMemberId() {
+    }
+
+    public void findMemberPw() {
+    }
+
+    public void modifyMemberBy() {
+    }
+
+    public void removeMemberBy() {
+    }
+
+    public int logout() {
+    }
+
+    public void findMemberBy() {
+    }
+
+    public void findMemberAll() {
+    }
 }
