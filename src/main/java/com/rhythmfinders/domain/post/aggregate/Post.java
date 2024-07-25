f220c8a5eeba8ccbc41c1b5fb598f11d1de0001d
gilhan <rlfgks97@gmail.com>
2024-07-26 01:01:23 +0900

    fix: update post entity

--- src/main/java/com/rhythmfinders/domain/post/aggregate/Post.java
+++ src/main/java/com/rhythmfinders/domain/post/aggregate/Post.java
@@ -1,96 +1,14 @@
 package com.rhythmfinders.domain.post.aggregate;
 
-import java.lang.reflect.Member;
-import java.util.Date;
+import java.time.LocalDateTime;
 
 public class Post {
     public int postId;
     public String postTitle;
     public String postContents;
-    public Member member;
-    public Date createDate;
-    public Date updateDate;
-    public int viewed;
+    public String member;
+    public LocalDateTime createDate;
+    public LocalDateTime updateDate;
+    public int view;
 
-    public Post() {
-    }
-
-    public Post(int postId, String postTitle, String postContents, Member member, Date createDate, Date updateDate, int viewed) {
-        this.postId = postId;
-        this.postTitle = postTitle;
-        this.postContents = postContents;
-        this.member = member;
-        this.createDate = createDate;
-        this.updateDate = updateDate;
-        this.viewed = viewed;
-    }
-
-    @Override
-    public String toString() {
-        return "Post{" +
-                "postId=" + postId +
-                ", postTitle='" + postTitle + '\'' +
-                ", postContents='" + postContents + '\'' +
-                ", member=" + member +
-                ", createDate=" + createDate +
-                ", updateDate=" + updateDate +
-                ", viewed=" + viewed +
-                '}';
-    }
-
-    public int getPostId() {
-        return postId;
-    }
-
-    public void setPostId(int postId) {
-        this.postId = postId;
-    }
-
-    public String getPostTitle() {
-        return postTitle;
-    }
-
-    public void setPostTitle(String postTitle) {
-        this.postTitle = postTitle;
-    }
-
-    public String getPostContents() {
-        return postContents;
-    }
-
-    public void setPostContents(String postContents) {
-        this.postContents = postContents;
-    }
-
-    public Member getMember() {
-        return member;
-    }
-
-    public void setMember(Member member) {
-        this.member = member;
-    }
-
-    public Date getCreateDate() {
-        return createDate;
-    }
-
-    public void setCreateDate(Date createDate) {
-        this.createDate = createDate;
-    }
-
-    public Date getUpdateDate() {
-        return updateDate;
-    }
-
-    public void setUpdateDate(Date updateDate) {
-        this.updateDate = updateDate;
-    }
-
-    public int getViewed() {
-        return viewed;
-    }
-
-    public void setViewed(int viewed) {
-        this.viewed = viewed;
-    }
 }
