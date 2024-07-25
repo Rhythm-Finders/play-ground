eb935dc206fe2e78f6bce0e8379bb8e09b2d37e0
gilhan <rlfgks97@gmail.com>
2024-07-26 01:08:29 +0900

    feat: add basic method for post entity

--- src/main/java/com/rhythmfinders/domain/post/aggregate/Post.java
+++ src/main/java/com/rhythmfinders/domain/post/aggregate/Post.java
@@ -11,4 +11,86 @@ public class Post {
     public LocalDateTime updateDate;
     public int view;
 
+    public Post() {
+    }
+
+    public Post(int postId, String postTitle, String postContents, String member, LocalDateTime createDate, LocalDateTime updateDate, int view) {
+        this.postId = postId;
+        this.postTitle = postTitle;
+        this.postContents = postContents;
+        this.member = member;
+        this.createDate = createDate;
+        this.updateDate = updateDate;
+        this.view = view;
+    }
+
+    public int getPostId() {
+
+        return postId;
+    }
+
+    public void setPostId(int postId) {
+        this.postId = postId;
+    }
+
+    public String getPostTitle() {
+        return postTitle;
+    }
+
+    public void setPostTitle(String postTitle) {
+        this.postTitle = postTitle;
+    }
+
+    public String getPostContents() {
+        return postContents;
+    }
+
+    public void setPostContents(String postContents) {
+        this.postContents = postContents;
+    }
+
+    public String getMember() {
+        return member;
+    }
+
+    public void setMember(String member) {
+        this.member = member;
+    }
+
+    public LocalDateTime getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(LocalDateTime createDate) {
+        this.createDate = createDate;
+    }
+
+    public LocalDateTime getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(LocalDateTime updateDate) {
+        this.updateDate = updateDate;
+    }
+
+    public int getView() {
+        return view;
+    }
+
+    public void setView(int view) {
+        this.view = view;
+    }
+
+    @Override
+    public String toString() {
+        return "Post{" +
+                "postId=" + postId +
+                ", postTitle='" + postTitle + '\'' +
+                ", postContents='" + postContents + '\'' +
+                ", member='" + member + '\'' +
+                ", createDate=" + createDate +
+                ", updateDate=" + updateDate +
+                ", view=" + view +
+                '}';
+    }
 }
