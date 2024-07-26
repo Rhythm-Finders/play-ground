86cc060a61c72cdfd60e2de6f3d5307127ab898c
woosuk <giuseog@gmail.com>
2024-07-26 14:02:05 +0900

    feat: run추가, ProductService 연동 중
    수정 중입니다

--- src/main/java/com/rhythmfinders/domain/product/run/Application.java
+++ src/main/java/com/rhythmfinders/domain/product/run/Application.java
@@ -1,9 +1,157 @@
 package com.rhythmfinders.domain.product.run;
 
-public class Application {
-    public static void main(String[] args) {
+import com.rhythmfinders.domain.product.aggregate.Product;
+import com.rhythmfinders.domain.product.service.ProductService;
 
-        System.out.println("hello product");
-        System.out.println("hi bro");
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Application {
+    private static final ProductService ps = new ProductService();
+
+    public static void main(String[] args) {
+        /* 설명. 여기서 메뉴는 요구사항 명세이다.
+         * */
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while(true){
+            System.out.println("===== 상품 관리 프로그램 =====");
+            System.out.println("1. 모든 상품 정보 보기");
+            System.out.println("2. 상품 찾기");
+            System.out.println("3. 상품 가입");
+            System.out.println("4. 상품 삭제");
+            System.out.println("5. 상품 수정");
+            System.out.println("9. 프로그램 종료");
+            System.out.println("메뉴를 선택해 주세요: ");
+            int input = 0;
+            try {
+                input = Integer.parseInt(br.readLine());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            switch(input){
+                case 1: ps.findAllProducts(); break;
+                /* 설명. 입력이 필요해서 main에서 입력 메소드를 지정(chooseMemNo()같은)
+                 *       서비스 전에 입력을 받아야 하는 것이다.
+                 *  */
+                case 2: ps.findProductBy(chooseMemNo()); break;
+                case 3: ps.registProduct(signUp()); break;
+                case 4: ps.removeProduct(chooseMemNo()); break;
+                case 5: ps.modifyProduct(chooseMemNo(), modifyUp()); break;
+                case 9:
+                    System.out.println("상품관리 프로그램을 종료합니다. "); return; //메인 종료
+                default:
+                    System.out.println("번호를 잘못 입력하셨습니다.");
+            }
+        }
     }
+
+    /* 설명. 매개변수가 많아서, 가공 처리를 하는 것*/
+    private static Product signUp() {
+        Product newProduct = null;
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));        System.out.print("아이디를 입력하세요: ");
+        String id = br.readLine();
+
+        System.out.println("패스워드를 입력하세요. ");
+        String pwd = sc.nextLine();
+
+        System.out.println("나이를 입력하세요. ");
+        int age = sc.nextInt();
+
+        System.out.println("입력할 취미 개수를 입력하세요(숫자로 1이상): ");
+        int length = sc.nextInt();
+        sc.nextLine();  //버퍼의 개행문자 처리용
+
+        String[] hobbies = new String[length];
+
+        for (int i = 0; i < hobbies.length; i++) {
+            System.out.println((1+1) + "번째 취미를 입력하세요: ");
+            String input = sc.nextLine();
+            hobbies[i] = input;
+        }
+
+        System.out.println(("혈액형을 입력하세요(A, AB, B, 0): "));
+        String bloodType = sc.nextLine().toUpperCase();
+        BloodType bt = null;
+
+        switch (bloodType) {
+            case "A": bt = BloodType.A; break;
+            case "AB": bt = BloodType.AB; break;
+            case "B": bt = BloodType.B; break;
+            case "O": bt = BloodType.O; break;
+        }
+
+        /* 필기.
+         *  회원으로부터 회원 가입을 위한 정보를 입력받아 Member 타입객체 하나로 가공 처리할 기반 기술이 두 가지가 있다.
+         *  1. 생성자 방식(장: 한줄 코딩, 단, 따로 생성자 추가 및 생성자의 매개변수가 다소 늘어날 수 있음(리팩토링 참조)
+         *  2. setter 방식(장: 초기화 할 갯수 수정 용이, 가독성이 좋음, 단: 코드의 줄 수가 늘어남)
+         * */
+        new Member(id, pwd, age, hobbies);
+
+        newMember = new Member(id, pwd, age, hobbies);
+
+        return newMember;
+    }
+    /* 설명. 매개변수가 많아서, 가공 처리를 하는 것*/
+    private static Product modifyUp() {
+        Product newMember = null;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("아이디를 입력하세요: ");
+        String id = sc.nextLine();
+
+        System.out.println("패스워드를 입력하세요. ");
+        String pwd = sc.nextLine();
+
+        System.out.println("나이를 입력하세요. ");
+        int age = sc.nextInt();
+
+        System.out.println("입력할 취미 개수를 입력하세요(숫자로 1이상): ");
+        int length = sc.nextInt();
+        sc.nextLine();  //버퍼의 개행문자 처리용
+
+        String[] hobbies = new String[length];
+
+        for (int i = 0; i < hobbies.length; i++) {
+            System.out.println((1+1) + "번째 취미를 입력하세요: ");
+            String input = sc.nextLine();
+            hobbies[i] = input;
+        }
+
+        System.out.println(("혈액형을 입력하세요(A, AB, B, 0): "));
+        String bloodType = sc.nextLine().toUpperCase();
+        BloodType bt = null;
+
+        switch (bloodType) {
+            case "A": bt = BloodType.A; break;
+            case "AB": bt = BloodType.AB; break;
+            case "B": bt = BloodType.B; break;
+            case "O": bt = BloodType.O; break;
+        }
+
+        /* 필기.
+         *  회원으로부터 회원 가입을 위한 정보를 입력받아 Member 타입객체 하나로 가공 처리할 기반 기술이 두 가지가 있다.
+         *  1. 생성자 방식(장: 한줄 코딩, 단, 따로 생성자 추가 및 생성자의 매개변수가 다소 늘어날 수 있음(리팩토링 참조)
+         *  2. setter 방식(장: 초기화 할 갯수 수정 용이, 가독성이 좋음, 단: 코드의 줄 수가 늘어남)
+         * */
+        new Product(id, pwd, age, hobbies);
+
+        newMember = new Product(id, pwd, age, hobbies);
+
+        newMember.setBloodType(bt);
+
+        return newMember;
+    }
+
+    /* 설명. 회원 ID를 입력받아 반환하는 메소드 */
+    private static int chooseMemNo() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("회원의 번호를 입력하세요: ");
+        return sc.nextInt();
+    }
+
 }
+
--- src/main/java/com/rhythmfinders/domain/product/service/ProductService.java
+++ src/main/java/com/rhythmfinders/domain/product/service/ProductService.java
@@ -1,4 +1,21 @@
 package com.rhythmfinders.domain.product.service;
 
+import com.rhythmfinders.domain.product.aggregate.Product;
+
 public class ProductService {
+    public void findAllProducts() {
+
+    }
+
+    public void findProductBy(int i) {
+    }
+
+    public void registProduct(Product product) {
+    }
+
+    public void removeProduct(int i) {
+    }
+
+    public void modifyProduct(int i, Product product) {
+    }
 }
