dae23d4462e96520cbd03862ec06cf24c50a27b4
gilhan <rlfgks97@gmail.com>
2024-07-27 17:34:16 +0900

    feat: add MainRunner

--- src/main/java/com/rhythmfinders/domain/post/PostApplication.java
+++ src/main/java/com/rhythmfinders/domain/post/PostApplication.java
@@ -1,6 +1,6 @@
 package com.rhythmfinders.domain.post;
 
-public class Application {
+public class PostApplication {
     public static void main(String[] args) {
 
         System.out.println("hello post");
